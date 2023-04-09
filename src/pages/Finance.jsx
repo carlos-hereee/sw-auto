06575823e42e13d9efcd53eb37a08bcec0290cdb
ch04937 <97hernandez.c@gmail.com>
2023-04-09 16:41:01 -0700

    create finance page

--- src/pages/Finance.jsx
+++ src/pages/Finance.jsx
@@ -2,6 +2,12 @@ const Finance = () => {
   return (
     <div>
       <h2>Financing Availible</h2>
+      <p>In Just 3 Easy Steps</p>
+      <h3>STEP1: Browse Vehicle</h3>
+      <h3>STEP2: Fill out credit application</h3>
+      <h3>STEP3: Bring important documents</h3>
+      <p>2 recent pay stubbs</p>
+      <p>3 proff of address</p>
     </div>
   );
 };
